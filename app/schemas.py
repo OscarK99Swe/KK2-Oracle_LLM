c0033f8f72ceba2dde19253d7189c3c77f96a0fd
OscarK99Swe <oscar.kock99@gmail.com>
2026-06-03 13:18:10 +0200

    Schemas GET prep
    Setting up schemas.py for the GET endpoint requirment

--- app/schemas.py
+++ app/schemas.py
@@ -5,3 +5,6 @@ class UploadMetadataResponse(BaseModel):
     rows: int
     columns: List[str]
     dtypes: Dict[str, str]
+
+class StatsResponse(BaseModel):
+    summary: Dict[str, Dict[str, float]]
