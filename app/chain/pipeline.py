7061bb4944a8bd8638ed8d76f24fe702aa40d8d8
OscarK99Swe <oscar.kock99@gmail.com>
2026-06-03 14:58:48 +0200

    Created local AI Pipeline
    Created a local AI Pipeline within App/Chain/Pipeline.py using SmolLM2-1.3B

---
+++ app/chain/pipeline.py
@@ -0,0 +1,28 @@
+from transformers import pipeline
+import functools
+
+@functools.lru_cache(maxsize=1)
+def load_text_pipeline():
+    print("Loading the SmolLM-model... Thank you for your patience, sigma")
+    
+    generator = pipeline(
+        "text-generation", 
+        model="HuggingFaceTB/SmolLM2-1.3B-Instruct",
+        device_map="auto"
+    )
+    return generator
+
+def generate_ai_answer(prompt: str) -> str:
+    generator = load_text_pipeline()
+    
+    outputs = generator(
+        prompt, 
+        max_new_tokens=150, 
+        temperature=0.3, 
+        do_sample=True
+    )
+    
+    generated_text = outputs[0]["generated_text"]
+    
+    answer = generated_text.replace(prompt, "").strip()
+    return answer
