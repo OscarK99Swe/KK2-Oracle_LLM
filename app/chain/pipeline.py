cd2ff31b67821aa62e557e1e3cda955578c4f92f
OscarK99Swe <oscar.kock99@gmail.com>
2026-06-03 19:24:51 +0200

    Simplified pipeline.py
    simplified the pipeline.py

--- app/chain/pipeline.py
+++ app/chain/pipeline.py
@@ -1,37 +1,12 @@
-from transformers import pipeline
-import functools
+from app.chain.steps import PromptBuilder, LLMRunner, ResponseParser
+from app.schemas import PromptBuilderInput, AskResponse
 
-@functools.lru_cache(maxsize=1)
-def load_text_pipeline():
-    print("Loading the SmolLM-model... Thank you for your patience, sigma")
+def generate_ai_answer_via_chain(question: str, stats_str: str) -> AskResponse:
+    prompt_builder = PromptBuilder()
+    llm_runner = LLMRunner()
+    response_parser = ResponseParser()
     
-    generator = pipeline(
-        "text-generation", 
-        model="HuggingFaceTB/SmolLM2-1.7B-Instruct",
-        device="cpu",
-        trust_remote_code=True
-    )
-    return generator
-
-def generate_ai_answer(prompt: str) -> str:
-    generator = load_text_pipeline()
+    chain = prompt_builder | llm_runner | response_parser
     
-    messages = [
-        {"role": "system", "content": "You are a helpful AI data analyst."},
-        {"role": "user", "content": prompt}
-    ]
-    
-    outputs = generator(
-        messages, 
-        max_new_tokens=150,
-        temperature=0.3, 
-        do_sample=True,
-        clean_up_tokenization_spaces=False
-    )
-    
-    answer = outputs[0]["generated_text"][-1]["content"].strip()
-    
-    if not answer:
-        return "The Oracle is speechless! (Model returned an empty string, sorry brochaco)"
-        
-    return answer
+    chain_input = PromptBuilderInput(question=question, stats_str=stats_str)
+    return chain.invoke(chain_input)
